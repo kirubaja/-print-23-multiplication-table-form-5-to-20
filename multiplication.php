221c978a3feb13ff017dd349709c20bc8d306236
kirubaja <152260859+kirubaja@users.noreply.github.com>
2023-11-30 12:18:43 +0530

    Add files via upload

---
+++ multiplication.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php    
+$num = 23;  
+for($i=5; $i<=20; $i++)   
+{
+$product = $i*$num;
+echo "$num * $i = $product" ;   
+echo '<br>';	 
+}  
+?> 
+ 
+</body>
+</html>
